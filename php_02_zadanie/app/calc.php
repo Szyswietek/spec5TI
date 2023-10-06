2e15e418ae3dfaee11b0c44c8db59b87105e2313
Szyswietek <144422256+Szyswietek@users.noreply.github.com>
2023-10-06 09:19:25 +0200

    Add files via upload

---
+++ php_02_zadanie/app/calc.php
@@ -0,0 +1,91 @@
+<?php
+// KONTROLER strony kalkulatora
+require_once dirname(__FILE__).'/../config.php';
+
+// W kontrolerze niczego nie wysyła się do klienta.
+// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
+// Parametry do widoku przekazujemy przez zmienne.
+
+// 1. pobranie parametrów
+function getParams(&$kwota,&$rata,&$operacja){
+	$kwota =isset($_REQUEST['kwota']) ? $_REQUEST['kwota']: null;
+	$rata = isset($_REQUEST['rata']) ? $_REQUEST['rata']: null;
+	$operacja = isset($_REQUEST['operacja']) ? $_REQUEST['operacja']: null;
+}
+// 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+function validate(&$kwota,&$rata,&$operacja,&$messages){
+	// sprawdzenie, czy parametry zostały przekazane
+	if ( ! (isset($kwota) && isset($rata) && isset($operacja))) {
+		//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
+		return false;
+	}
+
+	// sprawdzenie, czy potrzebne wartości zostały przekazane
+	if ( $kwota == "") {
+		$messages [] = 'Nie podano kwoty';
+	}
+	if ( $rata == "") {
+		$messages [] = 'Nie podano liczby liczby rat';
+	}
+	if ( $operacja == "") {
+		$messages [] = 'Nie podano liczby oprocentowania';
+	}
+
+	//nie ma sensu walidować dalej gdy brak parametrów
+	if (count ( $messages ) != 0) return false;
+
+	// sprawdzenie, czy $x i $y są liczbami całkowitymi
+	if (! is_numeric( $kwota )) {
+		$messages [] = 'Kwota nie jest liczbą całkowitą';
+	}
+	
+	if (! is_numeric( $rata )) {
+		$messages [] = 'Rata wartość nie jest liczbą całkowitą';
+	}
+
+	if (! is_numeric( $operacja )) {
+		$messages [] = 'Operacja nie jest liczbą całkowitą';
+	}		
+	
+	if (count ( $messages ) != 0) return false;
+	else return true;
+
+}
+
+function procces(&$kwota,&$rata,&$operacja,&$messages,&$wynik3){
+	global $role;
+
+
+
+	
+	//konwersja parametrów na int
+	$kwota = intval($kwota);
+	$rata = intval($rata);
+    $operacja = intval($operacja);
+	//wykonanie operacji
+
+    $procent=$operacja*0.01;
+    $wynik1=$kwota*$procent;
+    $wynik2=$kwota+$wynik1;
+    $wynik3= $wynik2/$rata;
+
+
+}
+	//definicja zmiennych kontrolera
+	$kwota = null;
+	$rata = null;
+	$operacja = null;
+	$wynik3 = null;
+	$messages = array();
+
+	//pobierz parametry i wykonaj zadanie jeśli wszystko w porządku
+	getParams($kwota,$rata,$operacja);
+	if ( validate($kwota,$rata,$operacja,$messages) ) { // gdy brak błędów
+		procces($kwota,$rata,$operacja,$messages,$wynik3);
+	}
+
+
+// 4. Wywołanie widoku z przekazaniem zmiennych
+// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
+//   będą dostępne w dołączonym skrypcie
+include 'calc_view.php';
---
+++ php_02_zadanie/app/calc_view.php
@@ -0,0 +1,44 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator</title>
+<link rel="stylesheet" href="<?php print(_APP_URL);?>/app/main.css">
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
+<label style="color: white;">Kwota:</label>  
+	<input id="kwota" type="number" name="kwota" value="<?php isset($kwota)?print($kwota):""; ?>"/><br/>
+	<label style="color: white;">Liczba rat:</label> 
+	<input type = "number" id = "rata" name = "rata" value = "<?php isset($rata)?print($rata):""; ?>"/><br/>
+	<label style="color: white;">Oprocentowanie:</label> 
+	<input type = "number" id = "op" name="operacja" value="<?php isset($op)?print($op):""; ?>"/>%<br/>
+	<br />
+	
+	<input style = "background-color:black;" type="submit" value="Oblicz" />
+	
+</form>	
+
+<?php
+//wyświeltenie listy błędów, jeśli istnieją
+if (isset($messages)) {
+	if (count ( $messages ) > 0) {
+		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+		foreach ( $messages as $key => $msg ) {
+			echo '<li>'.$msg.'</li>';
+		}
+		echo '</ol>';
+	}
+}
+?>
+
+<?php if (isset($wynik3)){ ?>
+<div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: black; width:300px;">
+<?php echo 'Jedna rata bedzie kosztowac: ' . round($wynik3); ?>
+</div>
+<?php } ?>
+
+</body>
+</html>
