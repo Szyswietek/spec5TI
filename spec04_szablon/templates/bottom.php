c7e798c56b3a03aee8c671a0f01a43680d46e2b6
Szyswietek <144422256+Szyswietek@users.noreply.github.com>
2024-01-03 16:31:36 +0100

    Add files via upload

---
+++ spec04_szablon/templates/bottom.php
@@ -0,0 +1,13 @@
+</div><!-- content -->
+
+<div class="footer">
+	<p>
+		<?php out($page_footer); if (!isset($page_footer)) {  ?> <?php } ?>
+	</p>
+	<p>
+		Widok oparty na stylach <a href="https://chat.openai.com/" target="_blank">Chat GPT</a>.
+	</p>
+</div>
+
+</body>
+</html>
